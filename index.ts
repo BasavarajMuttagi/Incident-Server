ff39c47a4184022db15d5e568738ad76f7feb7db
BasavarajMuttagi <basavaraj2770@gmail.com>
2025-02-09 19:20:40 +0530

    add WebSocket support; refactor server to use httpServer for socket connections and update export

--- index.ts
+++ index.ts
@@ -2,24 +2,35 @@ import { clerkMiddleware } from "@clerk/express";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
 import ComponentRouter from "./src/routes/component.route";
 import IncidentRouter from "./src/routes/incident.route";
 import SubscriberRouter from "./src/routes/subscriber.route";
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
+const httpServer = createServer(app);
+export const io = new Server(httpServer, { cors: { origin: "*" } });
 app.use(clerkMiddleware());
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello");
 });
-
 app.use("/api/v1/component", ComponentRouter);
 app.use("/api/v1/incident", IncidentRouter);
 app.use("/api/v1/subscriber", SubscriberRouter);
-app.listen(port, async () => {
+
+io.on("connection", async (socket) => {
+  console.log(socket.id, "connected");
+
+  socket.on("disconnect", async () => {
+    console.log("User disconnected");
+  });
+});
+httpServer.listen(port, async () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
-export default app;
+export default httpServer;
